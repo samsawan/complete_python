8abf548336ae58cf67ce60ccdc59a470317b401d
Sam Sawan <ssawan@onshift.com>
2018-02-08 16:33:18 -0500

    simplfying things a bit

--- milestone_projects/tic_tac_toe/tic_tac_toe.py
+++ milestone_projects/tic_tac_toe/tic_tac_toe.py
@@ -1,7 +1,7 @@
 player_one = {}
 player_two = {}
 EMPTY_ROW = [' ', ' ', ' ']
-board = {'1': EMPTY_ROW, '2': EMPTY_ROW, '3': EMPTY_ROW}
+board = {1: EMPTY_ROW, 2: EMPTY_ROW, 3: EMPTY_ROW}
 
 def welcome():
 	print 'Welcome to Tic Tac Toe!'
@@ -17,24 +17,18 @@ def render_board():
 	global board
 	# print board
 	LINE = '-----'
-	print ('{0}|{1}|{2}').format(board['1'][0], board['1'][1], board['1'][2])
+	print ('{0}|{1}|{2}').format(board[1][0], board[1][1], board[1][2])
 	print LINE
-	print ('{0}|{1}|{2}').format(board['2'][0], board['2'][1], board['2'][2])
+	print ('{0}|{1}|{2}').format(board[2][0], board[2][1], board[2][2])
 	print LINE
-	print ('{0}|{1}|{2}').format(board['3'][0], board['3'][1], board['3'][2])
+	print ('{0}|{1}|{2}').format(board[3][0], board[3][1], board[3][2])
 
 def take_turn(player):
 	global board
 	choice = raw_input('Please enter the coordinates of the board you want\n\tFormatted like 1,2: ')
-	final_choices = choice.split(',')
-	print board
-	row_choice = final_choices[0]
-	row_to_update = board[row_choice]
-	row_to_update[int(final_choices[1]) - 1] = player['symbol']
-	print row_to_update
-	print board[row_choice]
-	# board[row_choice] = row_to_update
-	print board
+	row_input, column_input = map(lambda choice: int(choice), choice.split(','))
+	board[row_input][column_input] = player['symbol']
+
 
 def play():
 	global player_one, player_two
