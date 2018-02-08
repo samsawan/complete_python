e3a40ebc389bd3ceebdab81376ff2a6f319cd9e0
Sam Sawan <ssawan@onshift.com>
2018-02-08 16:38:27 -0500

    fix empty_row bug
    list of lists cause why not
    no longer EMPTY_ROW referencing the same object and changing it

--- milestone_projects/tic_tac_toe/tic_tac_toe.py
+++ milestone_projects/tic_tac_toe/tic_tac_toe.py
@@ -1,7 +1,6 @@
 player_one = {}
 player_two = {}
-EMPTY_ROW = [' ', ' ', ' ']
-board = {1: EMPTY_ROW, 2: EMPTY_ROW, 3: EMPTY_ROW}
+board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
 
 def welcome():
 	print 'Welcome to Tic Tac Toe!'
@@ -17,15 +16,15 @@ def render_board():
 	global board
 	# print board
 	LINE = '-----'
-	print ('{0}|{1}|{2}').format(board[1][0], board[1][1], board[1][2])
+	print('{0}|{1}|{2}').format(board[0][0], board[0][1], board[0][2])
 	print LINE
-	print ('{0}|{1}|{2}').format(board[2][0], board[2][1], board[2][2])
+	print('{0}|{1}|{2}').format(board[1][0], board[1][1], board[1][2])
 	print LINE
-	print ('{0}|{1}|{2}').format(board[3][0], board[3][1], board[3][2])
+	print('{0}|{1}|{2}').format(board[2][0], board[2][1], board[2][2])
 
 def take_turn(player):
 	global board
-	choice = raw_input('Please enter the coordinates of the board you want\n\tFormatted like 1,2: ')
+	choice = raw_input('Please enter the coordinates of the board you want\n\tFormatted like 0,1: ')
 	row_input, column_input = map(lambda choice: int(choice), choice.split(','))
 	board[row_input][column_input] = player['symbol']
 
